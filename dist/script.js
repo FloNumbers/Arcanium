64fcd3f7a94a721434b5aa0d40fd5fd292ecceab
FloNumbers <floriansole@gmail.com>
2021-01-24 18:47:14 +0100

    add non working featureBuy

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,17 @@
 var Currency = (function () {
     function Currency() {
         this.counter = document.getElementById('counter');
-        this.dust = 0;
+        this.dust = 100;
         this.dustIncrement = 0;
         this.dustMultiplication = 1;
         this.books = 0;
         this.bookCounter = document.getElementById("books");
+        this.mapButton = document.getElementById("mapButton");
+        this.inventoryButton = document.getElementById("inventoryButton");
+        this.settingsButton = document.getElementById("settingsButton");
+        this.featuresBought = 0;
         document.getElementById("bookbuy").addEventListener("click", this.buyBook.bind(this));
+        document.getElementById("featureBuy").addEventListener("click", this.featureBuy.bind(this));
     }
     Currency.prototype.count = function () {
         this.dust += 1 + this.dustIncrement * this.dustMultiplication;
@@ -18,6 +23,27 @@ var Currency = (function () {
             this.books++;
             this.counter.innerHTML = "You have " + this.dust + " dust";
             this.bookCounter.innerHTML = "You have " + this.books + " books";
+        }
+    };
+    Currency.prototype.featureBuy = function () {
+        if (this.dust >= 30) {
+            this.dust -= 30;
+            if (this.featuresBought = 0) {
+                this.mapButton.innerHTML = "MAP";
+            }
+            else if (this.featuresBought = 1) {
+                this.inventoryButton.innerHTML = "INVENTORY";
+            }
+            else if (this.featuresBought = 2) {
+                this.settingsButton.innerHTML = "SETTINGS";
+            }
+            else {
+                var buttonsDiv = document.getElementById("buttons");
+                var newP = document.createElement("p");
+                newP.innerHTML = "You already bought all features";
+                buttonsDiv.appendChild(newP);
+            }
+            this.featuresBought += 1;
         }
     };
     return Currency;
--- src/start.ts
+++ src/start.ts
@@ -1,13 +1,18 @@
 class Currency {
     private counter = document.getElementById('counter');
-    private dust = 0;
+    private dust = 100;
     private dustIncrement = 0;
     private dustMultiplication = 1;
     private books = 0;
     private bookCounter = document.getElementById("books");
+    private mapButton = document.getElementById("mapButton");
+    private inventoryButton = document.getElementById("inventoryButton");
+    private settingsButton = document.getElementById("settingsButton");
+    private featuresBought = 0;
     
     constructor() {
-        document.getElementById("bookbuy").addEventListener("click", this.buyBook.bind(this))
+        document.getElementById("bookbuy").addEventListener("click", this.buyBook.bind(this));
+        document.getElementById("featureBuy").addEventListener("click", this.featureBuy.bind(this));
     }
 
     public count() {
@@ -21,6 +26,25 @@ class Currency {
             this.books ++;
             this.counter.innerHTML = "You have " + this.dust + " dust";
             this.bookCounter.innerHTML = "You have " + this.books + " books";
+        }
+    }
+
+    public featureBuy() {
+        if (this.dust >= 30) {
+            this.dust -= 30;
+            if (this.featuresBought = 0) {
+                this.mapButton.innerHTML = "MAP";
+            } else if (this.featuresBought = 1) {
+                this.inventoryButton.innerHTML = "INVENTORY";
+            } else if (this.featuresBought = 2) {
+                this.settingsButton.innerHTML = "SETTINGS";
+            } else {
+                let buttonsDiv = document.getElementById("buttons");
+                let newP = document.createElement("p");
+                newP.innerHTML = "You already bought all features";
+                buttonsDiv.appendChild(newP);
+            }
+            this.featuresBought += 1;
         }
     }
   }
